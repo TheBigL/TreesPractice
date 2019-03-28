3922cf4f10540b008066647d5e9e9926b569181c
TheBigL <lmohamed1@studentmail.nait.ca>
2019-03-28 13:43:32 -0600

    Building the Node Class (Constructor and getting started with the Insert class)

--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,14 @@
 public class Node
 {
+    private Node next;
+    private int data;
 
+    public Node(int data) {
+        this.data = data;
+    }
+
+    public void insert(int value)
+    {
+
+    }
 }
