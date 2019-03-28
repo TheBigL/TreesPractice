8d99df93bbe52a9e7b60f5d2ec42833643b8a73a
TheBigL <lmohamed1@studentmail.nait.ca>
2019-03-28 17:09:18 -0600

    Putting the Object Code inside the main class

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,61 @@ public class Main
         private Node left;
         private Node right;
         private int data;
+
+        private Node(int value)
+        {
+            data = value;
+            left = null;
+            right = null;
+        }
+
+        private boolean insert(int val) {
+            boolean added = false;
+            if (this == null) {
+                this.data = val;
+                return true;
+            }
+            else {
+                if (val < this.data) {
+                    if (this.left == null) {
+                        this.left = new Node(val);
+                        return true;
+                    }
+                    else
+                        added = this.left.insert(val);
+                }
+                else if (val > this.data) {
+                    if (this.right == null) {
+                        this.right = new Node(val);
+                        return true;
+                    }
+                    else
+                        added = this.right.insert(val);
+                }
+            }
+            return added;
+        }
+
+
+        private boolean find(int val) {
+            boolean found = false;
+            if (this == null)
+                return false;
+            else {
+                if (val == this.data)
+                    return true;
+                else if (val < this.data && this.left != null)
+                    found = this.left.find(val);
+                else if (val > this.data && this.right != null)
+                    found = this.right.find(val);
+            }
+            return found;
+        }
+
+        
     }
+
+
 
 
 
