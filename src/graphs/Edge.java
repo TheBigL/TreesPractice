81e0c9a9b12b857b03a3bf04f4c4f08685e07645
TheBigL <lmohamed1@studentmail.nait.ca>
2019-04-03 15:03:15 -0600

    Adds Grpahs to the List of things to Work on.

---
+++ src/graphs/Edge.java
@@ -0,0 +1,8 @@
+package graphs;
+
+public class Edge
+{
+    public GraphNode start;
+    public GraphNode end;
+    public double weight;
+}
---
+++ src/graphs/GraphNode.java
@@ -0,0 +1,12 @@
+package graphs;
+
+import java.util.List;
+
+public class GraphNode
+{
+    public String name;
+    public List<Edge> connections;
+
+
+}
+
