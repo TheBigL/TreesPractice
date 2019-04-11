cd40267dab6f636969743bcc172589408a89ce78
TheBigL <lmohamed1@studentmail.nait.ca>
2019-04-11 11:29:31 -0600

    Building the Graph

--- src/graphs/Graph.java
+++ src/graphs/Graph.java
@@ -1,4 +1,27 @@
 package graphs;
 
-public class Graph {
+import java.util.LinkedList;
+
+public class Graph
+{
+    int V; //Number of Vertices
+
+    LinkedList<Integer>[] adj; // adjacency lists
+
+    //Constructor
+    Graph(int V)
+    {
+        this.V = V;
+        adj = new LinkedList[V];
+
+        for (int i = 0; i < adj.length; i++)
+            adj[i] = new LinkedList<Integer>();
+
+    }
+
+    //To add an edge to graph
+    void addEdge(int v, int w)
+    {
+        adj[v].add(w); // Add w to v’s list.
+    }
 }
