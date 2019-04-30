c32ab41249f98f05a6c943a4053777cf13be5f58
TheBigL <lmohamed1@studentmail.nait.ca>
2019-04-30 17:50:22 -0600

    Expanding the Vertex System

--- src/TravellingSalesmanProblem.java
+++ src/TravellingSalesmanProblem.java
@@ -22,6 +22,32 @@ public class TravellingSalesmanProblem
         Random xGenerator = new Random(seed);
         Random yGenerator = new Random(2 * seed);
 
+        for(int i = 0; i < n; i++)
+        {
+            Vertex v = generateUniqueCoordinate(xGenerator, yGenerator);
+        }
+
+    }
+
+    public Vertex generateUniqueCoordinate(Random xGenerator, Random yGenerator)
+    {
+        // generate a random x and y
+        int x = xGenerator.nextInt(n);
+        int y = yGenerator.nextInt(n);
+        Vertex v = new Vertex(x, y);
+        boolean isUnique = true;
+
+        // check if x does not already exist in vertices
+        if (vertices.size() > 0) {
+            for (int j = 0; j < vertices.size(); j++) {
+                if (vertices.get(j).x() == x) {
+                    // if x already exists, start over (generate new coordinates)
+                    v = generateUniqueCoordinate(xGenerator, yGenerator);
+                }
+            }
+        }
+
+        return v;
     }
 
 
--- src/graphs/Vertex.java
+++ src/graphs/Vertex.java
@@ -7,6 +7,25 @@ public class Vertex
     public String name;
     public List<Edge> connections;
 
+    private final int x;
+    private final int y;
+    private int number;
+    private double dist;
 
+    public Vertex(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+        this.number = -1;
+        this.dist = -1;
+    }
+
+    public int x() {
+        return x;
+    }
+
+    public int y() {
+        return y;
+    }
 }
 
