dd508bc56266122a77f3e9bc71b244d96b17cf8a
TheBigL <lmohamed1@studentmail.nait.ca>
2019-04-30 17:37:39 -0600

    Picking the right variables

--- src/TravellingSalesmanProblem.java
+++ src/TravellingSalesmanProblem.java
@@ -1,4 +1,15 @@
+import graphs.Vertex;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
 public class TravellingSalesmanProblem
 {
+    int n;
+    private ArrayList<Vertex> vertices;
+    private double[][] adjacencyMatrix;
+    private DecimalFormat df = new DecimalFormat();
+
+
 
 }
