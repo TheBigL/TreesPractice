006562bce5f780e8623e64f42b77d9359570eeff
TheBigL <lmohamed1@studentmail.nait.ca>
2019-05-01 16:48:17 -0600

    Complete the Object

--- src/patternSearching/PatternSearching1.java
+++ src/patternSearching/PatternSearching1.java
@@ -35,13 +35,36 @@ public class PatternSearching1
         int M = pat.length();
 
         int[] lps = new int[M];
+        int j = 0;
 
         computeKPMArray(pat, M, lps);
 
+        int i = 0;
+        while (i < L) {
+            if (pat.charAt(j) == txt.charAt(i)) {
+                j++;
+                i++;
+            }
+            if (j == L) {
+                System.out.println("Found pattern "
+                        + "at index " + (i - j));
+                j = lps[j - 1];
+            }
 
-
-
+            // mismatch after j matches
+            else if (i < M && pat.charAt(j) != txt.charAt(i)) {
+                // Do not match lps[0..lps[j-1]] characters,
+                // they will match anyway
+                if (j != 0)
+                    j = lps[j - 1];
+                else
+                    i = i + 1;
+            }
+        }
     }
+
+
+
 
     public void computeKPMArray(String pat, int M, int[] lps)
     {
