3abdb4c805fe840bed2e72c210336aecdc0fc492
TheBigL <lmohamed1@studentmail.nait.ca>
2019-05-01 10:00:43 -0600

    patternSearch method

--- src/patternSearching/PatternSearching1.java
+++ src/patternSearching/PatternSearching1.java
@@ -2,8 +2,28 @@ package patternSearching;
 
 public class PatternSearching1
 {
-    public void patternSearch()
+    public void patternSearch(String txt, String pat)
     {
+        int L = txt.length();
+        int M = pat.length();
+
+        for(int i = 0; i <= L - M; i++)
+        {
+            int j;
+
+            for(j = 0; j < M; j++)
+            {
+                if(txt.charAt(i + j) != pat.charAt(j))
+                {
+                    break;
+                }
+
+                if (j == M)
+                {
+                    System.out.println("Pattern found at index " + i);
+                }
+            }
+        }
 
     }
 }
